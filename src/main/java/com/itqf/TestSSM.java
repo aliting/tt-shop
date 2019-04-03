525dc9dffde668c30ad7fd9c7f99947c9b3bb951
aliting <liliting_yx@163.com>
2019-04-03 15:33:08 +0800

    加了删除用户代码

--- src/main/java/com/itqf/TestSSM.java
+++ src/main/java/com/itqf/TestSSM.java
@@ -41,6 +41,8 @@ package com.itqf;
 public class TestSSM {
     public static void main(String[] args) {
         System.out.println("git test!!!!");
+        System.out.println("git test!!!!  branch dev!!!!");
+
     }
 
 
